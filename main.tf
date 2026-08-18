b47cf6730ccb9a054e6891b99822b6fd1bd3d05c
rdevrops-stack <rdevrops@gmail.com>
2026-08-18 21:27:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,137 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "demo" {
+  name     = "terraform-demo-rg"
+  location = "Central India"
+
+  tags = {
+    Environment = "Dev"
+    Project     = "Terraform"
+  }
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "demo" {
+  name                = "terraform-demo-vnet"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet
+resource "azurerm_subnet" "demo" {
+  name                 = "terraform-demo-subnet"
+  resource_group_name  = azurerm_resource_group.demo.name
+  virtual_network_name = azurerm_virtual_network.demo.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "demo" {
+  name                = "terraform-demo-public-ip"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "demo" {
+  name                = "terraform-demo-nic"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.demo.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.demo.id
+  }
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "demo" {
+  name                = "terraform-demo-nsg"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with NIC
+resource "azurerm_network_interface_security_group_association" "demo" {
+  network_interface_id      = azurerm_network_interface.demo.id
+  network_security_group_id = azurerm_network_security_group.demo.id
+}
+
+# Azure Linux VM
+resource "azurerm_linux_virtual_machine" "foo" {
+  name                = "TF-Instance"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  admin_password = "Azure@123456789!"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.demo.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  tags = {
+    Name        = "TF-Instance"
+    Environment = "Dev"
+  }
+}
+
+# Azure Storage Account
+resource "azurerm_storage_account" "demo" {
+  name                     = "tfdemo2026storage12345"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Name        = "Terraform-Storage"
+    Environment = "Dev"
+  }
+}
